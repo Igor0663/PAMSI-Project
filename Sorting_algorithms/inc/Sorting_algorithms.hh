a881bf215a170e409da00055e3737363689fd111
Igor0663 <igorzielinski3c@wp.pl>
2020-03-28 01:24:22 +0100

    fixed template parameters, heapsort kinda working

--- Sorting_algorithms/inc/Sorting_algorithms.hh
+++ Sorting_algorithms/inc/Sorting_algorithms.hh
@@ -3,16 +3,16 @@
 #include<functional>
 #include"Priority_queue.hh"
 
-template< typename T, class Compare = std::function<bool(T,T)> >
+template< typename T, class Compare = std::less<T> >
 void mergesort( T* tab_l, T* tab_r, Compare cmp = std::less<T>() );
 
-template< typename T, class Compare = std::function<bool(T,T)> >
+template< typename T, class Compare = std::less<T> >
 void quicksort( T* tab_l, T* tab_r, Compare cmp = std::less<T>() );
 
-template< typename T, class Compare = std::function<bool(T,T)> >
+template< typename T, class Compare = std::less<T> >
 void introsort( T* tab_l, T* tab_r, Compare cmp = std::less<T>() );
 
-template< typename T, class Compare = std::function<bool(T,T)> >
+template< typename T, class Compare = std::less<T> >
 void heapsort( T* tab_l, T* tab_r, Compare cmp = std::less<T>() );
 
 #include"../src/Sorting_algorithms.cpp"
