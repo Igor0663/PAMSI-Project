f8795d058eebedce8a35cbc3cf4ce2f1c6e71501
Igor0663 <igorzielinski3c@wp.pl>
2020-04-18 13:02:43 +0200

    Added priority queue representation from previous project.

---
+++ Graphs/inc/Priority_queue.hh
@@ -0,0 +1,28 @@
+#ifndef PRIORITY_QUEUE_HH
+#define PRIORITY_QUEUE_HH
+
+#include"Vector.hh"
+#include<functional>
+#include<iostream>
+
+template<typename T,class Comparator = std::greater<T> >
+class Priority_queue
+{
+	private:
+		Vector<T>* heap;
+		Comparator cmp;
+	public:
+		Priority_queue(){heap = new Vector<T>(); this->cmp = Comparator(); }
+		Priority_queue( const Comparator& cmp_) {heap = new Vector<T>(); this->cmp = cmp_; }
+		Priority_queue(const Priority_queue& Pqueue);
+		~Priority_queue();
+		const T& top() const;
+		bool empty() const {return this->heap->empty();}
+		unsigned int size() const{return this->heap->size();}
+		void push(const T& NewElement);
+		T pop();
+};
+
+#include"../src/Priority_queue.cpp"
+
+#endif
