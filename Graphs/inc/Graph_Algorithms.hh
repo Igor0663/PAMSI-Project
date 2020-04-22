a4f6fe54c43b75fd78c543558af4c1b46dd3eb9b
Igor0663 <igorzielinski3c@wp.pl>
2020-04-22 14:58:37 +0200

    Added BellmanFord algorithm implementation for Dijkstra testing purposes.

--- Graphs/inc/Graph_Algorithms.hh
+++ Graphs/inc/Graph_Algorithms.hh
@@ -6,21 +6,25 @@
 #include"Priority_queue.hh"
 
 template<typename Et>
-struct Dijkstra_result
+struct Find_dist_result
 {
-	Dijkstra_result(unsigned int n = 1, unsigned int S = 1){this->n = n; this->start_vertex = S; dist = new Et[n]; prev = new int[n];}
-	Dijkstra_result(const Dijkstra_result& D);
-	Dijkstra_result& operator =(const Dijkstra_result& D);
-	~Dijkstra_result(){delete[] dist; delete[] prev;}
+	Find_dist_result(unsigned int n = 1, unsigned int S = 1){this->n = n; this->start_vertex = S;neg_cycle = false; dist = new Et[n]; prev = new int[n];}
+	Find_dist_result(const Find_dist_result& D);
+	Find_dist_result& operator =(const Find_dist_result& D);
+	~Find_dist_result(){delete[] dist; delete[] prev;}
 	unsigned int start_vertex;
 	unsigned int n;
+	bool neg_cycle;
 	Et* dist;
 	int* prev;
 
 };
 
 template<typename Et>
-Dijkstra_result<Et> Dijkstra(Graph_base<Et>& G, unsigned int s, Et INF = (Et)(-1) );
+Find_dist_result<Et> Dijkstra(Graph_base<Et>& G, unsigned int s, Et INF = (Et)(-1), Et ZERO = (Et)(0) );
+
+template<typename Et>
+Find_dist_result<Et> BellmanFord(Graph_base<Et>& G, unsigned int S, Et INF = (Et)(-1), Et ZERO = (Et)(0) );
 
 #include"../src/Graph_Algorithms.cpp"
 
--- Graphs/src/Graph_Algorithms.cpp
+++ Graphs/src/Graph_Algorithms.cpp
@@ -1,8 +1,9 @@
 template< typename Et >
-Dijkstra_result<Et>::Dijkstra_result( const Dijkstra_result<Et>& D )
+Find_dist_result<Et>::Find_dist_result( const Find_dist_result<Et>& D )
 {
 	this->n = D.n;
 	this->start_vertex = D.start_vertex;
+	this->neg_cycle = D.neg_cycle;
 	this->dist = new Et[this->n];
 	this->prev = new int[this->n];	
 	for(unsigned int i = 0; i < this->n; i++)
@@ -13,10 +14,11 @@ Dijkstra_result<Et>::Dijkstra_result( const Dijkstra_result<Et>& D )
 }
 
 template<typename Et>
-Dijkstra_result<Et>& Dijkstra_result<Et>::operator =(const Dijkstra_result<Et>& D)
+Find_dist_result<Et>& Find_dist_result<Et>::operator =(const Find_dist_result<Et>& D)
 {
 	this->n = D.n;
 	this->start_vertex = D.start_vertex;
+	this->neg_cycle = D.neg_cycle;
 	delete [] this->dist;
 	delete [] this-> prev;
 	this->dist = new Et[this->n];
@@ -30,9 +32,9 @@ Dijkstra_result<Et>& Dijkstra_result<Et>::operator =(const Dijkstra_result<Et>& 
 }
 
 template<typename Et>
-Dijkstra_result<Et> Dijkstra(Graph_base<Et>& G, unsigned int s, Et INF)
+Find_dist_result<Et> Dijkstra(Graph_base<Et>& G, unsigned int s, Et INF, Et ZERO)
 {
-	Dijkstra_result<Et> r = Dijkstra_result<Et>(G.size(), s);
+	Find_dist_result<Et> r = Find_dist_result<Et>(G.size(), s);
 	Priority_queue< std::pair< Et, unsigned int>, std::less<std::pair<Et, unsigned int> > > Q;
 
 	for(unsigned int i = 0; i < G.size();i++)
@@ -41,7 +43,7 @@ Dijkstra_result<Et> Dijkstra(Graph_base<Et>& G, unsigned int s, Et INF)
 		r.prev[i] = -1;
 	}
 
-	r.dist[s - 1] = Et(0);
+	r.dist[s - 1] = ZERO;
 	Q.push( { r.dist[s -1], s} );
 	while(!Q.empty())
 	{
@@ -62,6 +64,47 @@ Dijkstra_result<Et> Dijkstra(Graph_base<Et>& G, unsigned int s, Et INF)
 				Q.push({r.dist[a - 1], a});
 			}	
 		}	
+	}
+	return r;
+}
+
+template<typename Et>
+Find_dist_result<Et> BellmanFord(Graph_base<Et>& G, unsigned int s, Et INF, Et ZERO)
+{
+	Find_dist_result<Et> r = Find_dist_result<Et>(G.size(), s);
+	for(unsigned int i = 0; i < G.size();i++)
+	{
+		r.dist[i] = INF;
+		r.prev[i] = -1;
+	}
+	r.dist[s - 1] = ZERO;
+	for(unsigned int i = 1; i < G.size();i++)
+	{
+		for(unsigned int j = 1; j <= G.size(); j++)
+		{
+			List<std::pair<unsigned int, Et> > L = G.incidentEdges(j);
+			for(auto it = L.begin(); it != L.end(); it++)
+			{
+				unsigned int a = (*it).first;
+				Et E_len = (*it).second;
+				if( r.dist[a -1] == INF or r.dist[a - 1] > r.dist[j - 1] + E_len )
+				{
+					r.dist[a - 1] = r.dist[j - 1] + E_len;
+					r.prev[a - 1] = j - 1;
+				}
+			}
+		}
+	}
+	for(unsigned int j = 1; j <= G.size(); j++)
+	{
+		List<std::pair<unsigned int, Et> > L = G.incidentEdges(j);
+		for(auto it = L.begin(); it != L.end(); it++)
+		{
+			unsigned int a = (*it).first;
+			Et E_len = (*it).second;
+			if( r.dist[a - 1] == INF or r.dist[a - 1] > r.dist[j - 1] + E_len )
+				r.neg_cycle = true;
+		}
 	}
 	return r;
 }
